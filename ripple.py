abf76cdbb3bbf28a8505f1928838c5bc77997a79
Charlie Bradley <charosbradley@gmail.com>
2018-01-15 17:06:29 -0600

    debugged thingsgit status

--- ripple.py
+++ ripple.py
@@ -1,32 +1,34 @@
 import math
 import unicornhathd
+import time
 
 #bloop
-r = 3
+r = 7
 
-center_x, center_y = 7
+center_x = 7
+center_y = 7
 
 original_pixel_x = center_x+r
 original_pixel_y = center_y
 
 count = 0
 
-for theta in range(0, 360):
-    pixel_x = center_x+r*cos(theta)
-    pixel_y = center_y+r*sin(theta)
+for theta in range(0, 628):
+    pixel_x = center_x+r*math.cos(theta/100)
+    pixel_y = center_y+r*math.sin(theta/100)
 
     if abs(pixel_x - original_pixel_x) > 1:
-        unicornhathd.set_pixel_hsv(origina_pixel_x, original_pixel_y, 0, .7, count / 8)
+        unicornhathd.set_pixel_hsv(original_pixel_x, original_pixel_y, 0, .7,1 )
         original_pixel_x = int(pixel_x)
         count = 0
 
-    if abs(pixel_y - original_pixel_y) > 1:
-        unicornhathd.set_pixel_hsv(origina_pixel_x, original_pixel_y, 0, .7, count / 8)
+    elif abs(pixel_y - original_pixel_y) > 1:
+        unicornhathd.set_pixel_hsv(original_pixel_x, original_pixel_y, 0, .7, 1)
         original_pixel_y = int(pixel_y)
         count = 0
 
     count += 1
-
-unicornhathd.show()
+    time.sleep(.01)
+    unicornhathd.show()
 #oooop
 #branch_test
